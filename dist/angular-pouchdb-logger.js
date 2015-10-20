88446efeb542d5c199ae1ec682a8520462c7c8f1
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-10-20 12:41:11 +0200

    Fix dis error

---
+++ dist/angular-pouchdb-logger.js
@@ -0,0 +1,240 @@
+/* angular-pouchdb-logger - Version 0.2.0, 20-10-2015
+ * 
+ * Enables logging to web database via pouchdb and $log delegate. The library is Ionic-aware and autoselect the best db for each platform
+ * 
+ * Copyright 2015  - Martin Reinhardt <contact@martinreinhardt-online.de>
+ * License MIT
+ */
+var core = angular.module('ngDbLogger.core', ['pouchdb']);
+
+// Core
+
+core.constant('ngDbLoggerConfig', {
+  dbName: 'logDB',
+  dbLogging: true,
+  debug: false,
+  trace: false
+});
+
+
+core.factory('dbService', ["pouchDB", function (pouchDB) {
+  'use strict';
+
+  var logDB;
+
+  // PUBLIC API
+  return function (dbName) {
+    if (!logDB) {
+      if (typeof ionic != 'undefined' && (ionic.Platform.isAndroid() || ionic.Platform.isWindowsPhone())) {
+        logDB = pouchDB(dbName, {adapter: 'idb', size: 50});
+      } else {
+        // default use websql
+        logDB = pouchDB(dbName, {adapter: 'websql', size: 50});
+      }
+    }
+    return logDB;
+  };
+}]);
+
+core.factory('dbLoggerService', ["$q", "$log", "dbService", function ($q, $log, dbService) {
+  'use strict';
+
+  var logConfig = $log.getConfig();
+  var readLogs = function (pLoglevel) {
+    var deferred = $q.defer(),
+      db = dbService(logConfig.dbName);
+    db.allDocs({
+      include_docs: true
+    })
+      .then(function (response) {
+        //, function (err, response) {
+
+        var logs = [];
+        for (var id in response.rows) {
+          var logEntry = response.rows[id].doc;
+          if (pLoglevel) {
+            // filter
+            if (pLoglevel == logEntry.level) {
+              logs.push(logEntry);
+            }
+          } else {
+            // no filter
+            logs.push(logEntry);
+          }
+        }
+        if (logs.length === 0) {
+          logs = null;
+        }
+        deferred.resolve(logs);
+      })
+      .catch(function (err) {
+        console.error('Error during writing log entries: ' + err);
+        deferred.reject(err);
+      });
+    return deferred.promise;
+  };
+  var deleteLogs = function () {
+    var deferred = $q.defer();
+    var db = dbService(logConfig.dbName);
+    db.destroy()
+      .then(function (response) {
+        console.info('Sucessfully cleared log database.');
+        deferred.resolve(response);
+      })
+      .catch(function (err) {
+        console.error('Error during clearing log database: ' + err);
+        deferred.reject(err);
+      });
+    return deferred.promise;
+  };
+
+  // PUBLIC API
+  return {
+    readLogData: function (pLoglevel) {
+      return readLogs(pLoglevel);
+    },
+    clearLogData: function () {
+      return deleteLogs();
+    }
+  };
+
+}]);
+
+/**
+ * @ngdoc service
+ * @name logger
+ * @module dbLog
+ * @description Provides logging service in browser db.
+ */
+core.provider('logger', function loggerProvider() {
+  'use strict';
+
+  var config = {},
+    log;
+  config.debug = false;
+  config.outputOnly = false;
+  config.trace = false;
+  config.dbName = 'log';
+
+  /**
+   * @ngdoc function
+   * @name logService#outputOnly
+   *
+   * @param {boolean} value to write to console only and not write to database (useful for testing)
+   */
+  this.outputOnly = function (value) {
+    config.outputOnly = !!value;
+  };
+
+  /**
+   * @ngdoc function
+   * @name logService#debugLogging
+   *
+   * @param {boolean} value to configure if debug entries should be logged
+   */
+  this.debugLogging = function (value) {
+    config.debug = !!value;
+  };
+  /**
+   * @ngdoc function
+   * @name logService#traceLogging
+   *
+   * @param {boolean} value to configure if trace entries should be logged
+   */
+  this.traceLogging = function (value) {
+    config.trace = !!value;
+  };
+  /**
+   * @ngdoc function
+   * @name logService#dbName
+   *
+   * @param {string} name of the database
+   */
+  this.dbName = function (name) {
+    config.dbName = name;
+  };
+  var writeLogEntry = function (dbService, pLogLevel, pArguments) {
+    var message = pArguments[0];
+    if (!config.outputOnly) {
+      var timestamp = new Date(),
+        db = dbService(config.dbName),
+        logEntry = {
+          timestamp: timestamp,
+          level: pLogLevel,
+          details: '' + message
+        };
+      if (db && db.bulkDocs) {
+        logEntry._id = '' + timestamp.getTime();
+        db.put(logEntry, function (error) {
+          if (error) {
+            log.error('Error during writing log entry: ' + error);
+          }
+        });
+      }
+    }
+  };
+  this.$get = ["$delegate", "dbService", "ngDbLoggerConfig", function Logger($delegate, dbService, ngDbLoggerConfig) {
+
+    config.dbName = ngDbLoggerConfig.dbName;
+    config.outputOnly = !ngDbLoggerConfig.dbLogging;
+    config.debug = ngDbLoggerConfig.debug;
+    if (ngDbLoggerConfig.debug) {
+      // enable couchDB debug
+      PouchDB.debug.enable('*');
+      PouchDB.debug.enable('pouchdb:find');
+    } else {
+      PouchDB.debug.disable();
+      PouchDB.debug.disable('pouchdb:find');
+    }
+    config.trace = ngDbLoggerConfig.trace;
+    log = $delegate;
+    return {
+      getConfig: function () {
+        return config;
+      },
+      log: function () {
+        log.info(arguments);
+        writeLogEntry(dbService, 'INFO', arguments);
+      },
+      warn: function () {
+        log.warn(arguments);
+        writeLogEntry(dbService, 'WARN', arguments);
+      },
+      info: function () {
+        log.info(arguments);
+        writeLogEntry(dbService, 'INFO', arguments);
+      },
+      error: function () {
+        log.error(arguments);
+        writeLogEntry(dbService, 'ERROR', arguments);
+      },
+      debug: function () {
+        if (config.debug) {
+          log.debug(arguments);
+          writeLogEntry(dbService, 'DEBUG', arguments);
+        }
+      },
+      trace: function () {
+        if (config.trace) {
+          log.trace(arguments);
+          writeLogEntry(dbService, 'TRACE', arguments);
+        }
+      }
+    };
+  }];
+});
+
+core.config(["$provide", "loggerProvider", "ngDbLoggerConfig", function ($provide, loggerProvider, ngDbLoggerConfig) {
+  'use strict';
+
+  // logging to pouchDB
+  $provide.decorator('$log', ['$delegate', 'dbService', 'ngDbLoggerConfig',
+    function ($delegate, dbService, ngDbLoggerConfig) {
+      /*if (loggerProvider.$get.length > 1) {
+       // solve uglify error
+       return loggerProvider.$get[1]($delegate);
+       } else {*/
+      return loggerProvider.$get($delegate, dbService, ngDbLoggerConfig);
+      // }
+    }]);
+}]);
