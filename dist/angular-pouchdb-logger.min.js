c573c619af90512bcee2fd764886650505daca0d
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-12-12 05:55:18 +0100

    chore(dependencies): update depdendencies

--- dist/angular-pouchdb-logger.min.js
+++ dist/angular-pouchdb-logger.min.js
@@ -1,4 +1,4 @@
-/* angular-pouchdb-logger - Version 0.3.7, 08-09-2016
+/* angular-pouchdb-logger - Version 0.3.8, 12-12-2016
  * 
  * Enables logging to web database via pouchdb and $log delegate. The library is Ionic-aware and autoselect the best db for each platform
  * 
